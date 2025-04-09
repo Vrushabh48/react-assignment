60b07c13a15282b1274077e7ff48a0d02a0665b4
Vrushabh Patil <vrushabhpatil4801@gmail.com>
2025-04-09 22:24:43 +0530

    Update App.jsx

--- react-assignment/src/App.jsx
+++ react-assignment/src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "https://cdn.skypack.dev/react";
-import LandingPage from "./pages/LandingPage.js";
-import ProductListing from "./pages/ProductListing.js";
-import CartPage from "./pages/CartPage.js";
-import Header from "./components/Header.js";
+import LandingPage from "./pages/LandingPage.jsx";
+import ProductListing from "./pages/ProductListing.jsx";
+import CartPage from "./pages/CartPage.jsx";
+import Header from "./components/Header.jsx";
 
 const App = () => {
   const [page, setPage] = useState("landing");
