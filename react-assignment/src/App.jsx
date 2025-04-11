57868358d230ff64fcae1accbb7d3b4d409c6809
Vrushabh Patil <vrushabhpatil4801@gmail.com>
2025-04-11 14:06:19 +0530

    update

--- react-assignment/src/App.jsx
+++ react-assignment/src/App.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "https://cdn.skypack.dev/react";
+import React, { useState } from "https://cdn.skypack.dev/react@18.2.0";
+import ReactDOM from "https://cdn.skypack.dev/react-dom@18.2.0";
 import LandingPage from "./pages/LandingPage.jsx";
 import ProductListing from "./pages/ProductListing.jsx";
 import CartPage from "./pages/CartPage.jsx";
-import Header from "./pages/Header.jsx";
+import Header from "./components/Header.jsx";
 
 const App = () => {
   const [page, setPage] = useState("landing");
